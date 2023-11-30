124199300e2cf8d96ebbb3ef3242ce1b3863bac5
jvadlamudi2 <152456004+jvadlamudi2@users.noreply.github.com>
2023-11-29 21:51:49 -0600

    Create preprocess_docs.py

---
+++ preprocess_docs.py
@@ -0,0 +1,16 @@
+# Import necessary libraries.
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+pdfLoader = DirectoryLoader('./data/')
+documents = []
+documents.extend(pdfLoader.load())
+print(f'You have {len(documents)} document(s) in your data folder.')
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(documents)
+print(len(documents))
+embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+db = Chroma.from_documents(documents, embeddings, persist_directory="./chroma_db/")
+db.persist()
